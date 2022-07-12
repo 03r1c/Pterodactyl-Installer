2c432c7fb240c85bf482d7f483d08f6881303ab0
03r1c <106050054+03r1c@users.noreply.github.com>
2022-07-12 10:50:59 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -475,10 +475,7 @@ composer(){
 files(){
     output "Downloading required files for Pterodactyl.."
     {
-    mkdir /var/www/pterodactyl
     cd /var/www/pterodactyl
-    curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
-    tar -xzvf panel.tar.gz
     chmod -R 755 storage/* bootstrap/cache/
     cp .env.example .env
     command composer install --no-dev --optimize-autoloader --no-interaction
